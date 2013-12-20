3333a5bcad1a59ed6093a29244a8e25066b611b0
laanak <laanak@gmail.com>
2013-12-20 18:26:12 -0500

    removed templates dir, and polished up some code

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,6 @@ exports.index = function(req, res){
 var request_module = require("request");
 var request = request_module.defaults({ json: true });
 
-
 var siteUrl = "https://api.imgur.com/3/gallery/hot/0";
 var clientID = "248a22763e9b17e";
 
@@ -20,25 +19,18 @@ var options = {
 	url: siteUrl,
 	headers : {
     	'Authorization': 'Client-ID ' + clientID
-
 	}
 };
 
-function callback(error, response, body) {
-	var images = new Array();
-	info = JSON.parse(body);
-	info.forEach(function(){
-		images
+exports.index = function(req, res){
+	request( options, function callback(error, response, body) {
+		var images = [];
+		for( var i in body.data ){
+			var imageObject = {};
+			imageObject.id = body.data[i].id;
+			imageObject.url = "http://i.imgur.com/" + imageObject.id + "b.jpg";
+			images.push(imageObject);
+		}
+		res.render('index', { theBody: images });
 	});
 }
-
-request( options, callback );
-
-/*
-images = []
-for each in response.body['data']:
-	imgID = each['id']
-	imgUrl = "http://i.imgur.com/" + imgID + "b.jpg"
-	img = "<img src=" + imgUrl + ">"
-	images.append( imgUrl )
-*/
