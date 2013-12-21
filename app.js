d57d351b54d0999c82c7546d34f8c3876cbe2c1a
Michael <m1reeder.mr@gmail.com>
2013-12-20 20:42:35 -0500

    added new package.json

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var express = require('express'),
 	path = require('path'),
 	passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy,
+	engine = require('ejs-locals'),
 	bcrypt = require('bcrypt'),
 	SALT_WORK_FACTOR = 10,
 	mongoose = require('mongoose'),
@@ -23,8 +24,12 @@ var express = require('express'),
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('ejs', engine);
 app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({secret: "crumbs"}));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
