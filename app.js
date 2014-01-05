878743bd9325b28ee0069cc6c5e2544bd38f3fdc
Michael <m1reeder.mr@gmail.com>
2014-01-05 17:39:25 -0500

    heroku redis stuff

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ var express = require('express'),
 
 if (process.env.REDISTOGO_URL) {
 	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-	var redis = require("redis").createClient(rtg.port, rtg.hostname);
-	redis.auth(rtg.auth.split(":")[1]);
+	var redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+	redisClient.auth(rtg.auth.split(":")[1]);
 } else {
 	var redisClient = require("redis").createClient();
 }
